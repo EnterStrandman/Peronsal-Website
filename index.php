9768653d400894785c79e6f3cbb8834a0d7d584c
Matt Strand <mstrandwest@gmail.com>
2018-10-31 22:33:30 -0400

    Add files via upload

---
+++ index.php
@@ -0,0 +1,35 @@
+<html>
+<head>
+	<?php include 'includes/header.php';?>
+</head>
+<body>
+	<div class="contentSixtyPercent">
+		<img src="resources/self.jpeg" alt="Headshot" id="profilePic">
+		<div class="rightOfProfilePicture" style="color:black">
+			<h1 style="font-weight: bold;">Matt Strand</h1>
+			<h3>Hometown: Fairmont, WV</h3>
+			<h3>Current Residence: Athens, WV</h3>
+			<h3>Education: Concord Unviersity 19'</h3>
+			<h3>Bachelors Degree: Computer Information Systems</h3>
+			<h3>Minor: Business Administration</h3>
+		</div>
+		<br><br><br><br><br>
+		<hr>
+		<!--this looks weird. maybe do it as a button and modal-->
+		<img src="resources/waterfall.jpg" id="waterfallPic">
+		<div class="hobbies">
+			<h1 style="font-weight: bold;">Hobbies</h1>
+			<ul style="font-size: 23px;">
+				<!--this could be turned into photo galleries-->
+				<li>Sports Cars</li>
+				<li>Fishing</li>
+				<li>Boating/Kayaking</li>
+				<li>Running/Biking</li>
+				<li>League of Legends</li>
+				<li>Minecraft</li>
+			</ul>
+			
+		</div>
+	</div>
+</body>
+</html>
---
+++ otherworks.php
@@ -0,0 +1,20 @@
+<html>
+<head>
+	<?php include 'includes/header.php';?>
+</head>
+<body>
+	<div class="contentSixtyPercent">
+		<img src="resources/runUSAscreenshot.jpeg" alt="Run USA home page" id="previewSites">
+		<div class="rightOfPreviewPicture" style="color:black">
+			<a href="otherworks/RunUSA"><h1 style="color: black"><strong>Run USA</strong></h1></a>
+			<p style="color: black; font-size: 20px;">This website was done for my web design class in the Fall of 2017. Since then, I have only had to update a picture with a corrected link and add a button to return to this page. Click the title to navigate to the website.</p>
+		</div>
+		<br><br><br><br><br>
+		<img src="resources/teachEasyScreenshot.jpeg" alt="Teach Easy login page" id="previewSites">
+		<div class="rightOfPreviewPicture" style="color:black">
+			<a href="otherworks/TeachEasy"><h1 style="color: black"><strong>Teach Easy</strong></h1></a>
+			<p style="color: black; font-size: 20px;">I worked with a group of 5 people (including myself) in a Software Engineering course to Create this site. We Also create a <a href="otherworks/TeachEasy/TeachEasyRequirements.pdf" style="color: rgb(127,127,127);"  target="_blank" >Requirements Document</a> in the course to better outline our intentions to the "clients".</p>
+		</div>
+	</div>
+</body>
+</html>
